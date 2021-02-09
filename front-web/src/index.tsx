0b06cd0d9e2c255a0feee0f4bc5b0d478951399f
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-02-09 11:55:16 -0300

    Add front-web

---
+++ front-web/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(
+  /*<h1>Hello, world!</h1>
+  // Esse "root" significa q o "Hello World" tah sendo inserido na "div" de id "root" q tah em "public -> index.html".
+  document.getElementById('root')*/
+
+  <App />,
+  document.getElementById('root')
+);
