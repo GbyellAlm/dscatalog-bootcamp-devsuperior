1f9068af83c8a417b5d0f13fe007ee4aa608de01
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-02-12 23:12:26 -0300

    Add Catalog and ProductCard (Catalog with bug in the layout (CSS bug)) Navbar still with bug in CSS

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import './styles.scss';
+import ProductCard from './components/ProductCard';
 
 const Catalog = () => (
-    <h1>Catalog</h1>
+    <div className="catalog-container">
+        <h1 className="catalog-title">Catálogo de produtos</h1>
+        <div className="catalog-products d-flex">
+            <ProductCard />
+            <ProductCard /> 
+            <ProductCard /> 
+            <ProductCard /> 
+            <ProductCard /> 
+            <ProductCard />
+            <ProductCard />
+            <ProductCard /> 
+            <ProductCard /> 
+        </div>
+    </div>
 );
 
 export default Catalog;
