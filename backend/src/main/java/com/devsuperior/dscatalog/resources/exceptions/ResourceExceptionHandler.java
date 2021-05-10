d15e4bff3617c98e36adcb9b1d0690ec86c4151e
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-05-10 12:03:42 -0300

    Custom validation response

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -41,6 +43,25 @@ public class ResourceExceptionHandler {
 		err.setError("Database exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		// "e.getBindingResult().getFieldErrors()" acessa a lista de erros do "MethodArgumentNotValidException" (lista interna de erros do 'Bean Validation'). Serve p/ q eu consiga gerar uma lista de erros na mensagem da resposta da requisicao.
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			// "f.getField()" pega o nome do campo, e "f.getDefaultMessage()" pega a msg de erro do campo.
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
 		return ResponseEntity.status(status).body(err);
 	}
 }
