c376e8019db649b142d019ddea037e77c435b700
GbyellAlm <gabriel_almeida@edu.univali.br>
2021-05-22 12:28:35 -0300

    Make login and save user session data

--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -1,18 +1,26 @@
 import axios, { Method } from 'axios';
+import qs from 'qs';
+import { CLIENT_ID, CLIENT_SECRET } from './auth';
 
 // UTILITARIO DO AXIOS PRA Q EU N PRECISE ESCREVER ESSE CODIGO TODA VEZ Q EU FOR UTILIZAR O AXIOS.
 
 type RequestParams = {
     method?: Method;
     url: string;
-    data?: object;
+    data?: object | string;
     params?: object;
+    headers?: object;
+}
+
+type LoginData = {
+    username: string;
+    password: string;
 }
 
 const BASE_URL = 'http://localhost:3000';
 // " method = 'GET'" eh o valor padrao do "method". 
 export const makeRequest = 
-({ method = 'GET', url, data, params }:RequestParams) => {
+({ method = 'GET', url, data, params, headers }:RequestParams) => {
     return axios({
         // Somente "method," pois o JS permite n escrever dnv valores q sao iguais as chaves.
         method,
@@ -21,6 +29,25 @@ export const makeRequest =
         // Dados q o backend vai receber de POSTS e UPDATES.
         data,
         // Parametros de req.
-        params
+        params,
+        headers
     });
+}
+
+export const makeLogin = ( loginData: LoginData ) => {
+    const token = `${CLIENT_ID}:${CLIENT_SECRET}`;
+
+    const headers = {
+        // "btoa(token)", esse "btoa" significa "byte to asc". Ele serve p/ criar o base 64 do client_id e do client_secret.
+        Authorization: `Basic ${window.btoa(token)}`,
+        // "'Content-Type'"" entre aspas simples pq no JS, qdo temos uma variavel q contem um caracter especial (traco, virgula, etc), temos q por ela entre aspas simples.
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    /*
+        * "...loginData" significa td q tem no "loginData";
+        * Ao inves de instalar o "qs" e seus tipos p/ fazer a req do login p/ o backend, eu poderia simplesmente fazer assim: "const payload = `username=${loginData.username}.com&password=${loginData.password}&grant_type=password`;" (q eh o q o "qs" se transforma por debaixo dos panos (olhar "Form Data -> View Source")). O "qs" foi usado p/ mostrar p/ gnt q ele existe, e q eh bastante usado pela comunidade (entretanto, segundo o prof "instalar libs so pra fazer uma coisinha simples dessas, n eh legal, mas dxa o codigo mais profissional").
+    */
+    const payload = qs.stringify({ ...loginData, grant_type: 'password' });
+    return makeRequest({ url: '/oauth/token', data: payload, method: 'POST', headers });
 }
--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,31 +1,48 @@
 import ButtonIcon from 'core/components/ButtonIcon';
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss'
+import { makeLogin } from 'core/utils/request';
+import { saveSessionData } from 'core/utils/auth';
 
 type FormData = {
-    email: string;
+    username: string;
     password: string;
 }
 
 const Login = () => {
     const { register, handleSubmit } = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
+    const history = useHistory();
 
     const onSubmit = (data: FormData) => {
         console.log(data);
         // Chamar a API de autenticacao
+        makeLogin(data).then(response => {
+            setHasError(false);
+            // "saveSessionData" salva os dados de autenticacao do usu no navegador. Isso eh necessario p/ poder acessar as outras coisas q precisam de login tb.
+            saveSessionData(response.data);
+            history.push('/admin')
+        }).catch(() => {
+            setHasError(true);
+        })
     }
     return (
         <div>
             <AuthCard title="login">
+                {hasError && (
+                    <div className="alert alert-danger mt-5">
+                        Usuário ou senha inválidos!
+                    </div>
+                )}
                 <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                    <input type="email" className="form-control input-base margin-bottom-30" placeholder="E-mail" name="email" ref={register}/>
-                    <input type="password" className="form-control input-base" placeholder="Senha" name="password" ref={register}/>
+                    <input type="email" className="form-control input-base margin-bottom-30" placeholder="E-mail" name="username" ref={register({ required: true })} />
+                    <input type="password" className="form-control input-base" placeholder="Senha" name="password" ref={register({ required: true })} />
                     <Link to="/admin/auth/recover" className="login-link-recover">Esqueci a senha?</Link>
                     <div className="login-submit">
-                        <ButtonIcon text="LOGAR"/>
+                        <ButtonIcon text="LOGAR" />
                     </div>
                     <div className="text-center">
                         <span className="not-registered">Não tem cadastro?</span>
